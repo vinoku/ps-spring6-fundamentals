66e8c6df60cb53f098a253d5d0cd7a371a09e8b3
vinoku <vinoku78@gmail.com>
2023-12-15 15:09:39 +0100

    use xml configs

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -2,10 +2,15 @@ package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        // SpeakerService service = new SpeakerServiceImpl();
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,10 +8,21 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new StubSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
