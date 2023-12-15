5d536937daa4c51f3bc2869cd241888b656f677a
vinoku <vinoku78@gmail.com>
2023-12-15 08:20:50 +0100

    first commit

---
+++ src/test/java/com/pluralsight/AppTest.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
